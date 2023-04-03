cba6954430f95a96ca9f3fd68add2802000f9d90
Lilianltd <lilian.letard@gmail.com>
2023-04-04 01:11:48 +0200

    RAZ

--- devenir_partenaire.php
+++ devenir_partenaire.php
@@ -1,53 +1,18 @@
 <!DOCTYPE html>
-<html>
-<head>
-   <title>Cartel 2024</title>
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.js"></script>
-</head >
+<link rel="stylesheet" href="stylesheet.css">
 
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown4
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action4</a></li>
-            <li><a class="dropdown-item" href="#">Another ac4tion</a></li>
-            <li><a class="dropdown-item" href="#">Something 4else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown2
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action2</a></li>
-            <li><a class="dropdown-item" href="#">Another2 action</a></li>
-            <li><a class="dropdown-item" href="#">Something2 else here</a></li>
-          </ul>
-        </li>
-      </ul>
+<?php 
+   include_once("navbar.html")
+?>
+
+<body class="d-flex flex-column min-vh-100">>
+    <div class="container mt-5 pt-3 h-100" style = "position : sticky">
+        <p>
+        test
+        </p>
     </div>
-  </div>
-</nav>
+</body>
 
+<?php 
+   include_once("footer.html")
+?>
