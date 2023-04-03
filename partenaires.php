ec115bd0ac5fdb6bf24e5078c410c226102baa11
lilian <lilian.letard@imt-atlantique.net>
2023-04-04 01:04:41 +0200

    j'ai avancé sur la navbar et le footer

--- partenaires.php
+++ partenaires.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
-<html>
-<style>
-   body{
-     background-color : #fff6d1;
-   }
-</style>
-<?php 
-   include("navbar.html")
-?> 
-<div class="container mt-5">
-   <p>
-      test
-   </p>
-</div>
-
+<link rel="stylesheet" href="stylesheet.css">
 
 <?php 
-   include("footer.html")
-?> 
-</html>
+   include_once("navbar.html")
+?>
+
+<body class="d-flex flex-column min-vh-100">>
+    <div class="container mt-5 pt-3 h-100" style = "position : sticky">
+        <p>
+        test
+        </p>
+    </div>
+</body>
+
+<?php 
+   include_once("footer.html")
+?>
