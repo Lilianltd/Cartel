e9c0af37a62b37b98b5ff307e6173e7c90aafa9f
Lilianltd <lilian.letard@gmail.com>
2023-04-19 16:24:15 +0200

    changement header (les sous menu sont en bleu)

--- devenir_partenaire.php
+++ devenir_partenaire.php
@@ -2,13 +2,14 @@
 <link rel="stylesheet" href="stylesheet.css">
 
 <?php 
-   include_once("navbar.html")
+   include_once("header.html")
 ?>
 
-<body class="d-flex flex-column min-vh-100">>
+<body class="d-flex flex-column min-vh-100">
     <div class="container mt-5 pt-3 h-100" style = "position : sticky">
-        <p>
-        test
+        <p class ="container row">
+            test
+        
         </p>
     </div>
 </body>
--- partenaires.php
+++ partenaires.php
@@ -2,14 +2,24 @@
 <link rel="stylesheet" href="stylesheet.css">
 
 <?php 
-   include_once("navbar.html")
+   include_once("header.html")
 ?>
 
-<body class="d-flex flex-column min-vh-100">>
+<body class="d-flex flex-column min-vh-100">
     <div class="container mt-5 pt-3 h-100" style = "position : sticky">
-        <p>
-        test
-        </p>
+        <div class = "row">
+            <div class = "col-sm flex-column float-end">
+                <div class = "row">
+                    test
+                </div>
+                <div class = "row">
+                    test
+                </div>
+            </div>
+            <div class = "col-sm flex-column float-end">
+                test
+            </div>
+        </div>
     </div>
 </body>
 
