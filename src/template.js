ff1efe2b5b33e349055bf63dce1063fd688f88bf
Lilianltd <lilian.letard@gmail.com>
2024-02-08 19:45:53 +0100

    modif footer

--- src/template.js
+++ src/template.js
@@ -95,10 +95,10 @@ function generateFooter(n) {
                 <div class="col-md-4 rowfooter" style='display:flex;flex-direction:column;align-items: center;justify-content: flex-start;'>
                     <h4 style="font-family: 'Designer',sans-serif;">Contact</h4>
                     <div>
-                        <svg width="2vw" height="3vh" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg height="5vh" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z" fill="white"/>
                         </svg>
-                        <a href="mailto:contact@cartel2024.com">contact@cartel2024.com</a>
+                        <a style="color: white; text-decoration: auto;" href="mailto:contact@cartel2024.com">contact@cartel2024.com</a>
                     </div>
                     <ul style="padding-top:1vh">
                         <li class="list-inline-item" style="width: 32px;height: 32px">
